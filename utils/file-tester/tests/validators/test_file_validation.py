18df4f724fea32e431e1be970f840c3b7d4a597f
Stephen A. Fuqua <stephen@safnet.com>
2021-01-22 10:26:58 -0600

    temporary error

--- utils/file-tester/tests/validators/test_file_validation.py
+++ utils/file-tester/tests/validators/test_file_validation.py
@@ -130,7 +130,8 @@ def describe_when_validating_users_file():
                 "LastModifiedDate",
                 "SourceCreateDate",
                 "SourceLastModifiedDate",
-                "Does not belong here",
+                # deliberately, temporarily, breaking this to test reporting in GitHub actions
+                # "Does not belong here",
             ]
             data = [
                 [
@@ -145,7 +146,7 @@ def describe_when_validating_users_file():
                     "9876-12-17 14:15:16",
                     "9876-12-18 13:14:15",
                     "9876-12-19 10:11:12",
-                    "does not belong here",
+                    #"does not belong here",
                 ]
             ]
             df = pd.DataFrame(columns=columns, data=data)
